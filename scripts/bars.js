9409eb3ff63d9c87571e03ed52b1f5e9f9fcab90
Sam Fries <samuelbfries@gmail.com>
2016-01-28 21:27:31 -0800

    Made style bars work

--- scripts/bars.js
+++ scripts/bars.js
@@ -1,34 +1,37 @@
-function build_bar(root, data, width, height) {
-	root.attr("width", width)
-		.attr("height", height);
+function build_bar(root, data, width, row_height) {
 	var key, count = 0, values = [], keys=[];
 	
 	map_obj(data, function(val, key) {
+		if (val === 0) {
+			return;
+		}
 		values.push(val);
 		keys.push(key);
 		count += 1;
 	});
+	
+	root.attr("width", width)
+		.attr("height", row_height * count);
+	
+	root.selectAll("*").remove();
 
 	var x = d3.scale.linear()
-						 .domain([0, d3.max(values)])
+						 .domain([0, d3.sum(values)])
 						 .range([0, width]);
-	var bar_height = height / count;
 	var bar = root.selectAll("g")
 				  .data(values)
 				  .enter()
 				  .append("g")
-				  .attr("transform", function(d, i) { return "translate(0," + i * bar_height + ")"; });
+				  .attr("transform", function(d, i) { return "translate(0," + i * row_height + ")"; });
+
 	bar.append("rect")
+		.attr("class", "bar")
 		.attr("width", x)
-		.attr("height", bar_height - 1);
+		.attr("height", row_height - 1);
 	bar.append('text')
-		.attr("x", function(d) { return x(d) - 3; })
-		.attr("y", bar_height / 2)
-		.attr("dy", ".35em")
-		.text(function(d) { return d; });
-	bar.append('text')
+		.attr("class", "bar_label")
 		.attr('x', 0)
-		.attr('y', bar_height / 2)
+		.attr('y', row_height / 2)
 		.attr('dy', ".35em")
 		.text(function(d, i) { return keys[i]; });
 }
